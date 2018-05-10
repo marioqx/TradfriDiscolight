993957f0139ade4929580bafb734216ba445c3ac
Mario <marioqx@users.noreply.github.com>
2018-05-10 22:01:01 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace VUmeter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 114);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Audio";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(178, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(74, 17);
+            this.panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "VU-Level";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 71);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(247, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(259, 206);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tradfri";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 168);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(240, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(160, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Connection Pre-shared Key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Connection Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tradfri-Gateway IP-Address";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 350);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::VUmeter.Properties.Settings.Default, "Title", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = global::VUmeter.Properties.Settings.Default.Title;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+using Tomidix.CSharpTradFriLibrary;
+using Tomidix.CSharpTradFriLibrary.Controllers;
+using Tomidix.CSharpTradFriLibrary.Models;
+
+namespace VUmeter
+{
+    public partial class Form1 : Form
+    {
+        private double level = 0;
+
+        private TradFriCoapConnector gatewayConnection = null;
+        private DeviceController dc = null;
+        private GroupController gc = null;
+
+        private const double FILTER_CONSTANT = 0.1;
+
+        Color[] colors = new Color[] { Color.FromArgb(0x4a, 0x41, 0x8a ),
+                                      Color.FromArgb( 0x8f, 0x26, 0x86 ),
+                                      Color.FromArgb( 0xdc, 0x4b, 0x31 ),
+                                      Color.FromArgb( 0xda, 0x5d, 0x41 ),
+                                      Color.FromArgb( 0x6c, 0x83, 0xba ),
+                                      Color.FromArgb( 0xd9, 0x33, 0x7c ),
+                                      Color.FromArgb( 0xe5, 0x73, 0x45 ),
+                                      Color.FromArgb( 0xe7, 0x88, 0x34 ),
+                                      Color.FromArgb( 0xa9, 0xd6, 0x2b ),
+                                      Color.FromArgb( 0xeb, 0xb6, 0x3e ),
+                                      Color.FromArgb( 0xc9, 0x84, 0xbb ),
+                                      Color.FromArgb( 0xd6, 0xe4, 0x4b ),
+                                      Color.FromArgb( 0xe4, 0x91, 0xaf ),
+                                      Color.FromArgb( 0xef, 0xd2, 0x75 ),
+                                      Color.FromArgb( 0xe8, 0xbe, 0xdd ),
+                                      Color.FromArgb( 0xf1, 0xe0, 0xb5 ),
+                                      Color.FromArgb( 0xf2, 0xec, 0xcf ),
+                                      Color.FromArgb( 0xdc, 0xf0, 0xf8 ),
+                                      Color.FromArgb( 0xea, 0xf6, 0xfb ),
+                                      Color.FromArgb( 0xf5, 0xfa, 0xf6 ) };
+        string[] colorStr = new string[] {  "Blue",
+                                            "Light Blue",
+                                            "Saturated Purple",
+                                            "Lime",
+                                            "Light Purple",
+                                            "Yellow",
+                                            "Saturated Pink",
+                                            "Dark Peach",
+                                            "Saturated Red",
+                                            "Cold sky",
+                                            "Pink",
+                                            "Peach",
+                                            "Warm Amber",
+                                            "Light Pink",
+                                            "Cool daylight",
+                                            "Candlelight",
+                                            "Warm glow",
+                                            "Warm white",
+                                            "Sunrise",
+                                            "Cool white"
+                                        };
+        string[] colorHex = new string[] {  "4a418a",
+                                            "8f2686",
+                                            "dc4b31",
+                                            "da5d41",
+                                            "6c83ba",
+                                            "d9337c",
+                                            "e57345",
+                                            "e78834",
+                                            "a9d62b",
+                                            "ebb63e",
+                                            "c984bb",
+                                            "d6e44b",
+                                            "e491af",
+                                            "efd275",
+                                            "e8bedd",
+                                            "f1e0b5",
+                                            "f2eccf",
+                                            "dcf0f8",
+                                            "eaf6fb",
+                                            "f5faf6"
+                                        };
+
+
+    public Form1()
+        {
+            InitializeComponent();
+
+            // Fill the Audio-devices selection
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var audiodevices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+            comboBox1.Items.AddRange( audiodevices.ToArray() );
+
+            // Fill the settings for the tradfri-gateway-connection
+            textBox3.Text = Properties.Settings.Default.GatewayIP;
+            textBox1.Text = Properties.Settings.Default.ConnectionName;
+            textBox2.Text = Properties.Settings.Default.GatewaySecret;
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            bool SaveSettings = ( Properties.Settings.Default.GatewayIP.Equals(textBox3.Text) ) &
+                                ( Properties.Settings.Default.ConnectionName.Equals(textBox1.Text)) &
+                                ( Properties.Settings.Default.GatewaySecret.Equals(textBox2.Text));
+
+            if ( !SaveSettings )
+            {
+                Properties.Settings.Default.GatewayIP = textBox3.Text;
+                Properties.Settings.Default.ConnectionName = textBox1.Text;
+                Properties.Settings.Default.GatewaySecret = textBox2.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+
+        private void LoadAllDevices()
+        {
+            GatewayController gwc = new GatewayController(gatewayConnection.Client);
+
+            comboBox2.Enabled = false;
+            comboBox2.BeginUpdate();
+            comboBox2.Items.Clear();
+
+            // Read the devices
+            foreach ( long deviceID in gwc.GetDevices() )
+            {
+                DeviceController dcl = new DeviceController(deviceID, gatewayConnection.Client);
+                TradFriDevice device = dcl.GetTradFriDevice();
+                comboBox2.Items.Add( device );
+            }
+
+            // Read the groups
+            foreach (long groupID in gwc.GetGroups())
+            {
+                GroupController gcl = new GroupController(groupID, gatewayConnection.Client);
+                TradFriGroup currentGroup = gcl.GetTradFriGroup();
+                comboBox2.Items.Add( currentGroup );
+            }
+
+            comboBox2.SelectedIndex = -1;
+            comboBox2.EndUpdate();
+            comboBox2.Enabled = true;
+        }
+
+
+        private void ConnectTradfri()
+        {
+            try
+            {
+                if (gatewayConnection == null )
+                {
+                    gatewayConnection = new TradFriCoapConnector( textBox1.Text, textBox3.Text, textBox2.Text);
+                    gatewayConnection.Connect();
+                }
+
+                // Query all devices and list in the comboBox.
+                LoadAllDevices();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Couldn't connect to TradFri gateway with provided settings");
+            }
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                bool update = false;
+                var device = (MMDevice)comboBox1.SelectedItem;
+                double index = Math.Round(device.AudioMeterInformation.MasterPeakValue * (colors.Length - 1));
+
+                // Update graphic VU-Meter in panel.
+                progressBar1.Value = (int)(Math.Round(device.AudioMeterInformation.MasterPeakValue * 100));
+
+                // Upodate the panel-background color and the color of the bulb, such that they change
+                // consistently and at a much lower rate than the VU-meter shown in progressBar1.
+                // This is necessary, because a too high update-rate crashes the tradfri-gateway and further
+                // may limit live-expectancy of the bulb.
+                if ((index - level >= 3) || (level - index >= 5))
+                {
+                    level = index;
+                    update = true;
+                }
+                else
+                {
+                    int old_level = (int)level;
+                    level = (1 - FILTER_CONSTANT) * level + FILTER_CONSTANT * index;
+                    if (old_level != (int)level) update = true;
+                }
+
+                // If required, then update the Tradfri-bulb color/dimmer and correspondigly the backcolor of the panel.
+                // For the individual buld, change the color. For the group change the dimmer.
+                if (update)
+                {
+                    if (dc != null)
+                    {
+                        panel1.BackColor = colors[(int)level];
+                        dc.SetColor(colorHex[(int)level]);
+                    }
+                    else if (gc != null)
+                    {
+                        int dimmer = (int)(level * 256 / colors.Length);
+                        gc.SetDimmer(dimmer);
+                        panel1.BackColor = Color.FromArgb(dimmer, dimmer, dimmer);
+                    }
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( button1.Text == "Start")
+            {
+                button1.Text = "Stop";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                level = 0;
+                progressBar1.Value = 0;
+                button1.Text = "Start";
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "Connecting";
+            button2.Enabled = false;
+            button2.Update();
+            ConnectTradfri();       // Note: This call hangs in case settings do not allow for successfully connection to the gateway.
+            button2.Text = "Connect";
+            button2.Enabled = true;
+            button2.Update();
+        }
+
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if ( comboBox2.SelectedIndex >= 0 )
+            {
+                if ( comboBox2.SelectedItem.GetType() == typeof( TradFriDevice ))
+                {
+                    var deviceToChangeProperties = (TradFriDevice) comboBox2.SelectedItem;
+                    dc = new DeviceController(deviceToChangeProperties.ID, gatewayConnection.Client);
+                    gc = null;
+                }
+                else if (comboBox2.SelectedItem.GetType() == typeof( TradFriGroup ))
+                {
+                    var deviceToChangeProperties = (TradFriGroup) comboBox2.SelectedItem;
+                    gc = new GroupController(deviceToChangeProperties.ID, gatewayConnection.Client);
+                    dc = null;
+                }
+                else
+                {
+                    dc = null;
+                    gc = null;
+                }
+            }
+        }
+    }
+}
